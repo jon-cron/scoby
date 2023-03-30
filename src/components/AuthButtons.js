a901065aae230a335e2ae2a2554f70435a54495a
jon-cron <jonathanacm95@gmail.com>
2023-03-30 16:42:49 -0600

    push

--- src/components/AuthButtons.js
+++ src/components/AuthButtons.js
@@ -3,19 +3,41 @@ import { useAuthContext } from "../context/auth_context.js";
 import Modal from "./Modal.js";
 const AuthButtons = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [login, setLogin] = useState(false);
   const { user } = useAuthContext();
   if (!user) {
     return (
       <div>
-        <button className="btn" onClick={() => setIsOpen(true)}>
+        <button
+          className="btn"
+          onClick={() => {
+            setLogin(true);
+            setIsOpen(true);
+          }}
+        >
           Login
         </button>
-        <button className="btn" onClick={() => setIsOpen(true)}>
+        <button
+          className="btn"
+          onClick={() => {
+            setLogin(false);
+            setIsOpen(true);
+          }}
+        >
           Register
         </button>
-        <Modal open={isOpen} onClose={() => setIsOpen(false)}>
-          I am a modal
-        </Modal>
+        <Modal
+          open={isOpen}
+          onClose={() => setIsOpen(false)}
+          login={login}
+          toggleLogin={() => setLogin(!login)}
+        ></Modal>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {user} <button>Logout</button>
       </div>
     );
   }
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,26 +1,112 @@
 import React from "react";
 import ReactDom from "react-dom";
 import styled from "styled-components";
-const Modal = ({ open, children, onClose }) => {
+const Modal = ({ open, onClose, login, toggleLogin }) => {
   if (!open) return null;
+
   return ReactDom.createPortal(
     <BackDrop>
-      <ModalBG className="modal">
-        {children}
-        <button onClick={onClose}>Close</button>
-      </ModalBG>
+      {login ? (
+        <ModalBG className="modal">
+          <h2>Login</h2>
+          <form>
+            <label>
+              <span>Email:</span>
+              <input type="text" placeholder="enter email" />
+            </label>
+            <label>
+              <span>Password:</span>
+              <input type="text" placeholder="enter password" />
+            </label>
+            <div>
+              <button className="btn">Login</button>
+              <button className="btn" onClick={onClose}>
+                Cancel
+              </button>
+            </div>
+          </form>
+          <p>
+            If you don't have an account click here:{" "}
+            <button className="btn" onClick={toggleLogin}>
+              register
+            </button>
+          </p>
+        </ModalBG>
+      ) : (
+        <ModalBG className="modal">
+          <h2>Register</h2>
+          <form>
+            <label>
+              <span>Email:</span>
+              <input type="text" placeholder="enter email" />
+            </label>
+            <label>
+              <span>Password:</span>
+              <input type="text" placeholder="enter password" />
+            </label>
+            <div>
+              <button className="btn">Login</button>
+              <button className="btn" onClick={onClose}>
+                Cancel
+              </button>
+            </div>
+          </form>
+          <p>
+            Do you already have an account?{" "}
+            <button className="btn" onClick={toggleLogin}>
+              login
+            </button>
+          </p>
+        </ModalBG>
+      )}
     </BackDrop>,
     document.getElementById("portal")
   );
 };
 const ModalBG = styled.div`
   position: fixed;
-  top: 50%;
-  left: 50%;
+  height: 50vh;
+  width: 60vw;
+  top: 30vh;
+  left: 20vw;
   transform: center;
   background: white;
-  padding: 50px;
   z-index: 1000;
+  padding: 10px;
+  border-radius: 5px;
+  h2 {
+    text-align: center;
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+  }
+  form label {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    margin-top: 8px;
+    height: 2rem;
+  }
+  form label span {
+    padding-right: 5px;
+    height: 100%;
+    text-align: center;
+    font-size: 20px;
+  }
+  form label input {
+    width: 80%;
+    height: 100%;
+  }
+  form div {
+    padding-top: 10px;
+    display: flex;
+    justify-content: space-evenly;
+  }
+  p {
+    text-align: center;
+  }
 `;
 const BackDrop = styled.div`
   position: fixed;
