e00b53137f368925589e898a5490389299c5c9e0
jon-cron <jonathanacm95@gmail.com>
2023-03-30 14:46:28 -0600

    user context

--- src/components/AuthButtons.js
+++ src/components/AuthButtons.js
@@ -2,13 +2,14 @@ import React from "react";
 import { useAuthContext } from "../context/auth_context.js";
 function AuthButtons() {
   const { user } = useAuthContext();
-  console.log(user);
-  return (
-    <div>
-      <button className="btn">Login</button>
-      <button className="btn">Register</button>
-    </div>
-  );
+  if (!user) {
+    return (
+      <div>
+        <button className="btn">Login</button>
+        <button className="btn">Register</button>
+      </div>
+    );
+  }
 }
 
 export default AuthButtons;
