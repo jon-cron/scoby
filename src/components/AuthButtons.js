cf33fe01c8d822b1b907b9c65b571661e7a0584f
jon-cron <jonathanacm95@gmail.com>
2023-03-30 13:03:45 -0600

    styling btns

--- src/components/AuthButtons.js
+++ src/components/AuthButtons.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function AuthButtons() {
-  return <div>AuthButtons</div>;
+  return (
+    <div>
+      <button className="btn">Login</button>
+      <button className="btn">Register</button>
+    </div>
+  );
 }
 
 export default AuthButtons;
