91f650fc2d9e47b7353641baa1ea933ae9712781
jon-cron <jonathanacm95@gmail.com>
2023-03-31 14:16:59 -0600

    push

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,20 @@
-import React from "react";
-import { AboutUs } from "../components";
+import React, { useState } from "react";
+import { AboutUs, Featured } from "../components";
 import { useCollection } from "../hooks/useCollection.js";
 const HomePage = () => {
   const { documents: products } = useCollection("kombuchas");
+  const [featured, setFeatured] = useState([]);
   console.log(products);
+  const getFeatured = () => {
+    let arr = [];
+    for (let index = 0; index < 3; index++) {
+      let getItem = products.fin;
+    }
+  };
   return (
     <>
       <AboutUs />
+      <Featured />
     </>
   );
 };
