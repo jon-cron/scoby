58920b57300ccd216b11a934c745f0a2b31f6e57
jon-cron <jonathanacm95@gmail.com>
2023-03-31 11:01:01 -0600

    about us

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -6,18 +6,24 @@ const AboutUs = () => {
   const AboutDiv = styled.div`
     background-color: #bfbfc1;
     display: flex;
-    grid-template-columns: 2fr 2fr;
+    align-items: center;
     justify-content: space-evenly;
-    padding: 30px;
+    padding: 30px 75px;
     img {
       width: 100%;
-      height: 40em;
+      height: 35em;
       object-fit: cover;
+      border-radius: 4px;
+      box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.5);
     }
     .text {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
       max-width: 40%;
       text-indent: 50px;
-      background: #fff;
+      height: 35em;
     }
   `;
   return (
@@ -28,25 +34,28 @@ const AboutUs = () => {
             <img src="https://plus.unsplash.com/premium_photo-1661776529978-0ea90437970a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8a29tYnVjaGF8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" />
           </div>
           <div className="text">
-            Do eu aute voluptate enim ex tempor reprehenderit non. Ea ad est
-            laborum sint. Ipsum veniam commodo do do. Ullamco dolor cupidatat
-            esse sit. Nulla quis in et culpa tempor culpa ea proident sunt. Ea
-            anim qui qui culpa anim cillum commodo aliquip exercitation.
-            Consequat quis id ea ut mollit eiusmod labore consequat sit minim
-            in.Veniam ea do aliqua velit pariatur eu magna voluptate labore
-            aliquip officia dolor laborum. Culpa cillum irure eiusmod sunt amet
-            ullamco dolore. Ullamco aliquip consequat eiusmod labore. Ullamco
-            officia mollit veniam eu fugiat in nostrud ad velit id ea. Officia
-            exercitation sint sunt ipsum irure excepteur excepteur dolor duis
-            Lorem. In consequat reprehenderit exercitation consectetur
-            incididunt commodo ex ullamco ipsum ut enim sunt. Elit duis
-            exercitation dolore tempor ad culpa fugiat laborum minim fugiat nisi
-            sint do.Qui nostrud officia officia cupidatat eu duis dolor nostrud.
-            Ex consequat dolore nostrud ipsum elit culpa ut sit eiusmod anim
-            consectetur. Ullamco est in labore non tempor do tempor ullamco esse
-            ipsum cupidatat. Duis duis consequat voluptate in occaecat voluptate
-            anim exercitation. Fugiat commodo exercitation veniam sit aute minim
-            magna ad consectetur deserunt.
+            <h2>About us</h2>
+            <span>
+              Do eu aute voluptate enim ex tempor reprehenderit non. Ea ad est
+              laborum sint. Ipsum veniam commodo do do. Ullamco dolor cupidatat
+              esse sit. Nulla quis in et culpa tempor culpa ea proident sunt. Ea
+              anim qui qui culpa anim cillum commodo aliquip exercitation.
+              Consequat quis id ea ut mollit eiusmod labore consequat sit minim
+              in.Veniam ea do aliqua velit pariatur eu magna voluptate labore
+              aliquip officia dolor laborum. Culpa cillum irure eiusmod sunt
+              amet ullamco dolore. Ullamco aliquip consequat eiusmod labore.
+              Ullamco officia mollit veniam eu fugiat in nostrud ad velit id ea.
+              Officia exercitation sint sunt ipsum irure excepteur excepteur
+              dolor duis Lorem. In consequat reprehenderit exercitation
+              consectetur incididunt commodo ex ullamco ipsum ut enim sunt. Elit
+              duis exercitation dolore tempor ad culpa fugiat laborum minim
+              fugiat nisi sint do.Qui nostrud officia officia cupidatat eu duis
+              dolor nostrud. Ex consequat dolore nostrud ipsum elit culpa ut sit
+              eiusmod anim consectetur. Ullamco est in labore non tempor do
+              tempor ullamco esse ipsum cupidatat. Duis duis consequat voluptate
+              in occaecat voluptate anim exercitation. Fugiat commodo
+              exercitation veniam sit aute minim magna ad consectetur deserunt.
+            </span>
           </div>
         </AboutDiv>
       )}
