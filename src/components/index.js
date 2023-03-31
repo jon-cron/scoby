5bcc29a445070dff6b7e626f5b1c45132e302792
jon-cron <jonathanacm95@gmail.com>
2023-03-31 14:17:03 -0600

    push

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ import Navbar from "./Navbar.js";
 import Modal from "./Modal.js";
 import AboutUs from "./AboutUs.js";
 import Footer from "./Footer.js";
-export { Navbar, Modal, AboutUs, Footer };
+import Featured from "./Featured.js";
+export { Navbar, Modal, AboutUs, Footer, Featured };
