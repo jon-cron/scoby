6ced024447eb8c594551a743a051c5530c2c522f
jon-cron <jonathanacm95@gmail.com>
2023-04-01 11:02:49 -0600

    rendering featured

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,11 +3,30 @@ import styled from "styled-components";
 
 const Product = ({ product }) => {
   const Wrapper = styled.div`
-    height: 10em;
+    height: 15em;
     width: 30%;
+    border-radius: 5px;
+    box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.4);
+    img {
+      height: 8em;
+      border-top-left-radius: 5px;
+      border-top-right-radius: 5px;
+      width: 100%;
+      object-fit: cover;
+      div {
+        display: flex;
+      }
+    }
   `;
   const { id, name, description, img } = product;
-  return <Wrapper>{id}</Wrapper>;
+  return (
+    <Wrapper>
+      <img src={img} alt="" />
+      <div>
+        <h6>{name}</h6>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Product;
