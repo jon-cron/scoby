760ac7dd4a60c965a43dbd97e8a29221bc44e90e
jon-cron <jonathanacm95@gmail.com>
2023-04-03 08:28:45 -0600

    context notes

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -1,5 +1,6 @@
+//imports
 import { createContext, useEffect, useReducer, useContext } from "react";
-
+//Local storage function check
 const getLocalStorage = () => {
   let cart = localStorage.getItem("cart");
   if (cart) {
@@ -8,23 +9,35 @@ const getLocalStorage = () => {
     return [];
   }
 };
+//initial state
 const initialState = {
   cart: getLocalStorage(),
   total_items: 0,
   total_amount: 0,
   shipping_fee: 500,
 };
+//creation of context
 const CartContext = createContext();
-
+//reducer section
+export const cartReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+//context/provider section
 export const CartContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(cartReducer, initialState);
   // SECTION functions
   //SECTION end
   return (
     // NOTE when you create functions you want access to place them in the value
-    <CartContext.Provider value={{ ...state }}>{children}</CartContext.Provider>
+    <CartContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </CartContext.Provider>
   );
 };
+//use context hook
 export const useCartContext = () => {
   const context = useContext(CartContext);
   if (!context) {
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { AuthContextProvider } from "./context/auth_context.js";
+import { CartContextProvider } from "./context/cart_context.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <CartContextProvider>
+        <App />
+      </CartContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
