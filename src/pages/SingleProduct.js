d6d3041f89970406e06aeb27ecf7a8d4c26b86d7
jon-cron <jonathanacm95@gmail.com>
2023-04-05 09:33:10 -0600

    push style

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,11 +1,45 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 import { useDocument } from "../hooks/useDocument.js";
 
 const SingleProduct = () => {
   const id = useParams().id;
   const { document } = useDocument("kombuchas", id);
   console.log(document);
+  const Wrapper = styled.main`
+    .product-center {
+      display: grid;
+      gap: 4rem;
+      margin-top: 2rem;
+    }
+    .price {
+      color: var(--clr-primary-5);
+    }
+    .desc {
+      line-height: 2;
+      max-width: 45em;
+    }
+    .info {
+      text-transform: capitalize;
+      width: 300px;
+      display: grid;
+      grid-template-columns: 125px 1fr;
+      span {
+        font-weight: 700;
+      }
+    }
+
+    @media (min-width: 992px) {
+      .product-center {
+        grid-template-columns: 1fr 1fr;
+        align-items: center;
+      }
+      .price {
+        font-size: 1.25rem;
+      }
+    }
+  `;
   return <div>SingleProduct</div>;
 };
 
